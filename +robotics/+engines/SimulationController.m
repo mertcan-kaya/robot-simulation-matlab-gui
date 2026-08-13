e4b2a9901b2d73f70c7c5e1e8f2bb423acf58206
Mertcan Kaya <mertcan.kaya@gmail.com>
2026-08-13 03:47:49 +0200

    Decoupling continues

--- +robotics/+engines/SimulationController.m
+++ +robotics/+engines/SimulationController.m
@@ -18,6 +18,104 @@ classdef SimulationController < handle
             obj.renderer = robotics.graphics.RobotRenderer(axesHandle);
         end
         
+        function setRobotModel(obj, robotId, eeAtt, customParams)
+            obj.model.robot_model = robotId;
+            obj.model.ee_att = eeAtt;
+            
+            % If it's a custom robot, apply its manual properties
+            if robotId == 0 && nargin > 3
+                obj.model.kin = customParams.kin;
+                obj.model.dyn.m_j = customParams.m_j;
+                obj.model.dyn.rj_jcj = customParams.rj_jcj;
+                obj.model.dyn.Ij_cj = customParams.Ij_cj;
+            end
+            
+            obj.initRobot();
+            obj.model.notifyUpdate();
+        end
+        
+        function toggleSprings(obj, isOn)
+            obj.model.dyn.spring_on = isOn;
+            obj.model.notifyUpdate();
+        end
+        
+        function toggleFriction(obj, isOn)
+            obj.model.dyn.friction_on = isOn;
+            obj.model.notifyUpdate();
+        end
+        
+        function updateForwardKinematics(obj, targetType, eulerSet)
+            if strcmp(targetType, 'ini')
+                q_pos = obj.model.ini.q_pos;
+            else
+                q_pos = obj.model.fin.q_pos;
+            end
+            
+            Ti = zeros(4,4,obj.model.kin.n+2);
+            Ti(:,:,1) = eye(4);
+            for j = 1:obj.model.kin.n
+                Ti(:,:,j+1) = Ti(:,:,j) * robotics.math.SO3R3_SE3(robotics.math.getRi_j(obj.model.kin.alpha_j(j), obj.model.kin.theta_O_j(j)+q_pos(j)), obj.model.kin.ri_j(:,:,j));
+            end
+            Ti(:,:,obj.model.kin.n+2) = Ti(:,:,obj.model.kin.n+1) * robotics.math.SO3R3_SE3(robotics.math.getRi_j(obj.model.kin.alpha_j(obj.model.kin.n+1), obj.model.kin.theta_O_j(obj.model.kin.n+1)), obj.model.kin.ri_j(:,:,obj.model.kin.n+1));
+            
+            [Re, t_pos] = robotics.math.SE3_SO3R3(Ti(:,:,obj.model.kin.n+2));
+            r_pos = robotics.math.getEulerPosVec(Re, eulerSet);
+            
+            if strcmp(targetType, 'ini')
+                obj.model.ini.Ti = Ti;
+                obj.model.ini.Re = Re;
+                obj.model.ini.t_pos = t_pos;
+                obj.model.ini.r_pos = r_pos;
+            else
+                obj.model.fin.Ti = Ti;
+                obj.model.fin.Re = Re;
+                obj.model.fin.t_pos = t_pos;
+                obj.model.fin.r_pos = r_pos;
+            end
+        end
+
+        function setJointPosition(obj, targetType, jointIndex, value, eulerSet)
+            if strcmp(targetType, 'ini')
+                obj.model.ini.q_pos(jointIndex) = value;
+            else
+                obj.model.fin.q_pos(jointIndex) = value;
+            end
+            obj.updateForwardKinematics(targetType, eulerSet);
+            obj.model.notifyUpdate();
+        end
+
+        function updateFinTimeTrj(obj, prcnt)
+            obj.model.tfin_trj = robotics.engines.TrajectoryEngine.computeTime(...
+                obj.model.ini.q_pos, obj.model.fin.q_pos, prcnt, obj.model.trj_profile, ...
+                obj.model.kin.q_velLim, obj.model.kin.q_accLim);
+            obj.model.notifyUpdate();
+        end
+        
+        function setTargetPosition(obj, targetType, xyzValues, eulerValues, eulerSet)
+            % Inverse kinematics update logic
+            Re_ref = robotics.math.getRotMatfromEA(eulerValues, eulerSet);
+            t_pos_ref = xyzValues;
+            
+            invGeoConfig.inv_geo_type = obj.model.inv_geo_type;
+            invGeoConfig.inv_geo_trn = obj.model.inv_geo_trn;
+            invGeoConfig.kp_inv = obj.model.kp_inv;
+            invGeoConfig.kr_inv = obj.model.kr_inv;
+            invGeoConfig.kp_trn = obj.model.kp_trn;
+            invGeoConfig.kr_trn = obj.model.kr_trn;
+            
+            if strcmp(targetType, 'ini')
+                qDes = robotics.engines.KinematicsEngine.inverseKinematics(obj.robot, invGeoConfig, obj.model.kin, Re_ref, t_pos_ref, obj.model.ini.q_pos);
+                obj.model.ini.q_pos = qDes;
+            else
+                qDes = robotics.engines.KinematicsEngine.inverseKinematics(obj.robot, invGeoConfig, obj.model.kin, Re_ref, t_pos_ref, obj.model.fin.q_pos);
+                obj.model.fin.q_pos = qDes;
+            end
+            
+            % Update kinematics matrices based on new q_pos
+            obj.updateForwardKinematics(targetType, eulerSet);
+            obj.model.notifyUpdate();
+        end
+        
         function initRobot(obj)
             % Instantiates the OOP Robot physics and loads the 3D meshes
             obj.robot = robotics.models.RobotFactory.create(obj.model.robot_model);
@@ -42,6 +140,35 @@ classdef SimulationController < handle
             obj.model.kin.ri_j = zeros(3,1,obj.model.kin.n+2);
             for j = 1:obj.model.kin.n+1
                 obj.model.kin.ri_j(:,:,j) = robotics.math.getri_j_vec(obj.model.kin.alpha_j(j), obj.model.kin.a_j(j), obj.model.kin.d_j(j));
+            end
+            
+            if ~isfield(obj.model.kin, 'r_posLim')
+                obj.model.kin.r_posLim = [-pi pi; -pi pi; -pi pi];
+            end
+            if ~isfield(obj.model.kin, 'r_velLim')
+                obj.model.kin.r_velLim = [-pi/4 pi/4];
+            end
+            if ~isfield(obj.model.kin, 'r_accLim')
+                obj.model.kin.r_accLim = [-pi/8 pi/8];
+            end
+            if ~isfield(obj.model.kin, 't_posLim')
+                obj.model.kin.t_posLim = [-1 1; -1 1; -1 1];
+            end
+            if ~isfield(obj.model.kin, 'q_posLim')
+                obj.model.kin.q_posLim = zeros(obj.model.kin.n,2);
+                obj.model.kin.q_velLim = zeros(obj.model.kin.n,2);
+                obj.model.kin.q_accLim = zeros(obj.model.kin.n,2);
+                for j = 1:obj.model.kin.n
+                    if obj.model.kin.j_type(j) == 1
+                        obj.model.kin.q_posLim(j,:) = [-pi, pi];
+                        obj.model.kin.q_velLim(j,:) = [-pi, pi];
+                        obj.model.kin.q_accLim(j,:) = [-pi, pi];
+                    else
+                        obj.model.kin.q_posLim(j,:) = [-1, 1];
+                        obj.model.kin.q_velLim(j,:) = [-1, 1];
+                        obj.model.kin.q_accLim(j,:) = [-1, 1];
+                    end
+                end
             end
             if ismethod(obj.robot, 'getInertialParameters')
                 new_dyn = obj.robot.getInertialParameters();
@@ -77,9 +204,11 @@ classdef SimulationController < handle
             % Initialize transformation matrices and orientations for ini and fin
             obj.model.ini.Ti = robotics.engines.KinematicsEngine.getTransMatrix(obj.model.TI_0, obj.model.kin.a_j, obj.model.kin.alpha_j, obj.model.kin.d_j, obj.model.kin.theta_O_j, obj.model.kin.j_type, obj.model.ini.q_pos);
             [obj.model.ini.Re, obj.model.ini.t_pos] = robotics.math.SE3_SO3R3(obj.model.ini.Ti(:,:,n+2));
+            obj.model.ini.r_pos = robotics.math.getEulerPosVec(obj.model.ini.Re, 1); % Assume eulerSet 1 (ZYZ) by default
             
             obj.model.fin.Ti = robotics.engines.KinematicsEngine.getTransMatrix(obj.model.TI_0, obj.model.kin.a_j, obj.model.kin.alpha_j, obj.model.kin.d_j, obj.model.kin.theta_O_j, obj.model.kin.j_type, obj.model.fin.q_pos);
             [obj.model.fin.Re, obj.model.fin.t_pos] = robotics.math.SE3_SO3R3(obj.model.fin.Ti(:,:,n+2));
+            obj.model.fin.r_pos = robotics.math.getEulerPosVec(obj.model.fin.Re, 1);
             
             % Update default control parameters from the robot object
             if ~isfield(obj.model.ctr, 'algo')
--- +robotics/+models/SimulationModel.m
+++ +robotics/+models/SimulationModel.m
@@ -35,8 +35,9 @@ classdef SimulationModel < handle
         ref;
         
         % End Effector special properties
-        qEIni;
-        qEDes;
+        % End Effector special properties
+        qEIni = 0;
+        qEDes = 0;
         
         % Inverse Kinematics Parameters
         inv_geo_type = 2; % 0: alg, 1: num, 2: hybrid
@@ -46,10 +47,32 @@ classdef SimulationModel < handle
         kp_trn = 0.003;
         kr_trn = 0.0001;
     end
+    events
+        ModelUpdated
+    end
     
     methods
         function obj = SimulationModel()
-            % Constructor (empty for now)
+            % Constructor - initialize strict data structures
+            obj.kin = struct();
+            obj.kin.g0 = -[0;0;9.81];
+            
+            obj.dyn = struct();
+            obj.dyn.spring_on = 0;
+            obj.dyn.friction_on = 0;
+            
+            obj.ini = struct();
+            obj.fin = struct();
+            obj.act = struct();
+            obj.des = struct();
+            obj.fbk = struct();
+            obj.ctr = struct();
+            obj.ref = struct();
+        end
+        
+        function notifyUpdate(obj)
+            % Helper to trigger the UI update event
+            notify(obj, 'ModelUpdated');
         end
     end
 end
